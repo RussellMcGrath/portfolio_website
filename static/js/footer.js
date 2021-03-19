f5f2ada4103d089c3c3659439de13303c0b6f15c
RussellMcGrath <mcgrath.russell@gmail.com>
2021-03-19 15:31:01 -0400

    changes

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,5 +1,8 @@
 var footer = d3.select("body").append("footer");
 
-var footerCode = "© Russell McGrath"
+// var footerCode = "© Russell McGrath"
+
+var footerCode = `© Russell McGrath\
+<div>Some Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>`
 
 footer.html(footerCode)
